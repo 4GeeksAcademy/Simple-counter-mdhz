9c310d93b94cfe9b1f75f57c71f2e81ff3c800b6
MDhz89 <mdjho89@gmail.com>
2024-12-18 01:48:52 +0000

    contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,21 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+// Import React into the bundle
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SimpleCounter from "./component/home.jsx"
+import '../styles/index.css';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
